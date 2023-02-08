b628972a2096c22398ce66938246d5d35de02327
gfhe <90042648+gfhe@users.noreply.github.com>
2023-02-08 07:07:33 +0000

    origin vector data

--- pre_build_graph.sh
+++ pre_build_graph.sh
@@ -1,3 +1,5 @@
 mkdir data
+wget ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz
+wget ftp://ftp.irisa.fr/local/texmex/corpus/gist.tar.gz
 wget -P data http://downloads.zjulearning.org.cn/nsg/sift_200nn.graph
 wget -P data http://downloads.zjulearning.org.cn/nsg/gist_400nn.graph
